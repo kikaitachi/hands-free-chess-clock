1c3f449f472c1ca11dfa4d2c4b8ea578f617a139
KIKAItachi <61438920+kikaitachi@users.noreply.github.com>
2023-12-31 20:33:12 +0200

    chore: create speech to text class

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 import audio_capture;
+import speech_to_text;
 import video_capture;
 
 int main() {
   AudioCapture audio_capture("hw:0");
+  SpeechToText speech_to_text;
   audio_capture.start();
   return 0;
 }
