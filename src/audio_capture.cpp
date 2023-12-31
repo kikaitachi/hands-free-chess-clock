d0e10e6b0a0c6cf156400d0da08cf579b20a2969
KIKAItachi <61438920+kikaitachi@users.noreply.github.com>
2023-12-31 17:51:32 +0200

    chore: set hardware parameters

--- src/audio_capture.cpp
+++ src/audio_capture.cpp
@@ -15,9 +15,26 @@ export class AudioCapture {
       fprintf(stderr, "Can't open audio device %s (%s)\n", device.c_str(), snd_strerror(err));
       exit(1);
     }
+    if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) {
+      fprintf(stderr, "Can't allocate hardware parameter structure (%s)\n", snd_strerror(err));
+      exit (1);
+    }
+    if ((err = snd_pcm_hw_params_any(capture_handle, hw_params)) < 0) {
+      fprintf(stderr, "Can't initialize hardware parameter structure (%s)\n", snd_strerror(err));
+      exit (1);
+    }
+    if ((err = snd_pcm_hw_params_set_access(capture_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+      fprintf(stderr, "Can't set access type (%s)\n", snd_strerror(err));
+      exit(1);
+    }
+    if ((err = snd_pcm_hw_params_set_format(capture_handle, hw_params, format)) < 0) {
+      fprintf(stderr, "Can't set sample format (%s)\n", snd_strerror(err));
+      exit (1);
+    }
   }
 
   void start() {
+    snd_pcm_start(capture_handle);
   }
  private:
   snd_pcm_t *capture_handle;
