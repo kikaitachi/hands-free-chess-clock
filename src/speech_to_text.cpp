dd89b45031fa2f83384da1c8ba136e6413ef7374
Mykolas Juraitis <mykolas@engineeredarts.co.uk>
2024-01-05 19:27:19 +0000

    fix: download model

--- src/speech_to_text.cpp
+++ src/speech_to_text.cpp
@@ -9,7 +9,7 @@ export class SpeechToText {
   SpeechToText() {
     struct whisper_context_params cparams;
     cparams.use_gpu = true;
-    ctx = whisper_init_from_file_with_params("base", cparams);
+    ctx = whisper_init_from_file_with_params("build/_deps/whisper.cpp-src/models/ggml-base.bin", cparams);
   }
  private:
   whisper_context* ctx;
