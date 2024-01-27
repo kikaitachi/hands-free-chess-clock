18273009294e42140f944c06c3aec793f4856304
Mykolas Juraitis <mykolas.juraitis@gmail.com>
2024-01-27 20:38:49 +0000

    feat: find convex polygons with 4 vertices

--- src/video_capture.cpp
+++ src/video_capture.cpp
@@ -1,5 +1,6 @@
 #include "logger.hpp"
 #include "video_capture.hpp"
+#include <opencv2/core/hal/interface.h>
 #include <filesystem>
 #include <thread>
 
@@ -17,6 +18,7 @@ void VideoCapture::start_game() {
   // Detect board
   std::lock_guard<std::mutex> guard(frame_mutex);
   cv::imwrite("images/start_game_original.jpg", frame);
+  cv::Mat markers(frame);
   cv::Mat gray;
   cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
   cv::imwrite("images/start_game_gray.jpg", gray);
@@ -26,6 +28,22 @@ void VideoCapture::start_game() {
   cv::Mat threshold;
   cv::adaptiveThreshold(blurred, threshold, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 11, 2);
   cv::imwrite("images/start_game_threshold.jpg", threshold);
+  std::vector<std::vector<cv::Point> > contours;
+  std::vector<cv::Vec4i> hierarchy;
+  cv::findContours(threshold, contours, hierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+  for (auto & contour : contours) {
+    cv::Mat approx;
+    approxPolyDP(contour, approx, 20, true);
+    if (approx.size().height == 4 && cv::isContourConvex(approx)) {
+      for (int i = 0; i < 4; i++) {
+        cv::Point pt1 = approx.at<cv::Point>(i, 0);
+        cv::Point pt2 = approx.at<cv::Point>((i + 1) % 4, 0);
+        cv::line(markers, pt1, pt2, {0, 255, 0}, 5, cv::LINE_AA);
+      }
+      //cv::polylines(markers, approx, true, {0, 255, 0}, 5, cv::LINE_AA);
+    }
+  }
+  cv::imwrite("images/start_game_markers.jpg", markers);
 }
 
 void VideoCapture::capture_frames() {
