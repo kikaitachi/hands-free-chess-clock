0d7a62f13b01941518d0ad3eaedc61093a889a26
Mykolas Juraitis <mykolas.juraitis@gmail.com>
2024-01-27 22:10:10 +0000

    feat: find top & bottom most lines

--- src/video_capture.cpp
+++ src/video_capture.cpp
@@ -1,11 +1,43 @@
 #include "logger.hpp"
 #include "video_capture.hpp"
+#include <algorithm>
 #include <cmath>
 #include <filesystem>
+#include <optional>
 #include <thread>
 #include <utility>
 
 using namespace std::chrono_literals;
+
+typedef std::pair<cv::Point, cv::Point> Line;
+
+struct {
+  bool operator()(Line a, Line b) const {
+    return std::min(a.first.y, a.second.y) < std::min(b.first.y, b.second.y);
+  }
+}
+line_top;
+
+struct {
+  bool operator()(Line a, Line b) const {
+    return std::max(a.first.y, a.second.y) < std::max(b.first.y, b.second.y);
+  }
+}
+line_bottom;
+
+struct {
+  bool operator()(Line a, Line b) const {
+    return std::min(a.first.x, a.second.x) < std::min(b.first.x, b.second.x);
+  }
+}
+line_left;
+
+struct {
+  bool operator()(Line a, Line b) const {
+    return std::max(a.first.x, a.second.x) < std::max(b.first.x, b.second.x);
+  }
+}
+line_right;
 
 VideoCapture::VideoCapture() {
   std::filesystem::create_directories("images");
@@ -32,18 +64,21 @@ void VideoCapture::start_game() {
   std::vector<std::vector<cv::Point> > contours;
   std::vector<cv::Vec4i> hierarchy;
   cv::findContours(threshold, contours, hierarchy, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+  std::optional<Line> topmost_line;
+  std::optional<Line> bottommost_line;
+  std::optional<Line> leftmost_line;
+  std::optional<Line> rightmost_line;
   for (auto & contour : contours) {
     cv::Mat approx;
     approxPolyDP(contour, approx, 20, true);
     if (approx.size().height == 4 && cv::isContourConvex(approx)) {
-      std::vector<std::pair<cv::Point, cv::Point>> horizontal_lines;
-      std::vector<std::pair<cv::Point, cv::Point>> vertical_lines;
+      std::vector<Line> horizontal_lines;
+      std::vector<Line> vertical_lines;
       std::vector<double> line_lengths;
       for (int i = 0; i < 4; i++) {
         cv::Point pt1 = approx.at<cv::Point>(i, 0);
         cv::Point pt2 = approx.at<cv::Point>((i + 1) % 4, 0);
-        std::pair<cv::Point, cv::Point> line;
-        //cv::line(markers, pt1, pt2, {0, 255, 0}, 5, cv::LINE_AA);
+        Line line;
         line_lengths.push_back(std::sqrt(std::pow(pt2.y - pt1.y, 2) + std::pow(pt2.x - pt1.y, 2)));
         double angle = std::atan2(pt2.y - pt1.y, pt2.x - pt1.x) * 180.0 / M_PI;
         if (
@@ -58,10 +93,20 @@ void VideoCapture::start_game() {
       }
       std::sort(line_lengths.begin(), line_lengths.end());
       if (horizontal_lines.size() == 2 && line_lengths[0] > line_lengths[3] * 0.45) {
+        std::sort(horizontal_lines.begin(), horizontal_lines.end(), line_top);
+        if (!topmost_line || line_top.operator()(topmost_line.value(), horizontal_lines[0])) {
+          topmost_line = horizontal_lines[0];
+        }
+        std::sort(horizontal_lines.begin(), horizontal_lines.end(), line_bottom);
+        if (!bottommost_line || !line_bottom.operator()(bottommost_line.value(), horizontal_lines[1])) {
+          bottommost_line = horizontal_lines[1];
+        }
         cv::polylines(markers, approx, true, {0, 255, 0}, 5, cv::LINE_AA);
       }
     }
   }
+  cv::line(markers, topmost_line.value().first, topmost_line.value().second, {255, 0, 0}, 5, cv::LINE_AA);
+  cv::line(markers, bottommost_line.value().first, bottommost_line.value().second, {255, 0, 0}, 5, cv::LINE_AA);
   cv::imwrite("images/start_game_markers.jpg", markers);
 }
 
