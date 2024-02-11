53800ea32029e02ee606a28dab550a78aa3b72fc
Mykolas Juraitis <mykolas.juraitis@gmail.com>
2024-02-11 19:01:23 +0000

    test: add test for making move

--- test/chess_engine_test.hpp
+++ test/chess_engine_test.hpp
@@ -1,7 +1,27 @@
 #include <gtest/gtest.h>
 #include "../src/chess_engine.hpp"
 
-TEST(PositionTest, InitialMoves) {
+int cell2index(std::string cell) {
+  return (cell[1] - (int)'1') * 8 + (cell[0] - (int)'a');
+}
+
+chess::Move move(std::string move) {
+  return {
+    cell2index(move.substr(0, 2)),
+    cell2index(move.substr(2, 4)),
+    chess::Figure::Empty
+  };
+}
+
+TEST(PositionTest, InitialPosition) {
   chess::Position position;
+  EXPECT_TRUE(position.white_turn);
   EXPECT_EQ(position.generate_legal_moves().size(), 20);
+}
+
+TEST(PositionTest, Move) {
+  chess::Position position;
+  chess::GameResult result = position.move(move("d2d4"));
+  EXPECT_EQ(result.winner, chess::Winner::None);
+  EXPECT_EQ(result.message, "d2d4");
 }
