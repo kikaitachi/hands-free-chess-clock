823f3dcb7b57727eb8fc63c4c31047a64962ec53
Mykolas Juraitis <mykolas.juraitis@gmail.com>
2024-02-29 18:21:40 +0000

    chore: download piper models

--- build.sh
+++ build.sh
@@ -5,10 +5,18 @@ set -e
 build_dir=build
 model_file="${build_dir}/_deps/whisper.cpp-src/models/ggml-small.en.bin"
 silero_model="models/silero_vad.onnx"
+piper_onnx="models/en_US-amy-medium.onnx"
+piper_json="${piper_onnx}.json"
 
 mkdir -p models
 if [ ! -f "${silero_model}" ]; then
   curl -s -S https://raw.githubusercontent.com/snakers4/silero-vad/master/files/silero_vad.onnx > "${silero_model}"
+fi
+if [ ! -f "${piper_onnx}" ]; then
+  curl -s -S https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/amy/medium/en_US-amy-medium.onnx > "${piper_onnx}"
+fi
+if [ ! -f "${piper_json}" ]; then
+  curl -s -S https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/amy/medium/en_US-amy-medium.onnx.json > "${piper_json}"
 fi
 
 if [ ! -d "${build_dir}" ]; then
