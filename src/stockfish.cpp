98a372e24616ab25ba4cedb31e5afba47b79c8fb
Mykolas Juraitis <mykolas@engineeredarts.co.uk>
2024-03-04 07:25:56 +0000

    chore: add files for Stockfish specific UCI

---
+++ src/stockfish.cpp
@@ -0,0 +1,1 @@
+#include "stockfish.hpp"
---
+++ src/stockfish.hpp
@@ -0,0 +1,10 @@
+#ifndef STOCKFISH_H_
+#define STOCKFISH_H_
+
+#include "uci.hpp"
+
+class Stockfish: protected UniversalChessInterface {
+
+};
+
+#endif  // STOCKFISH_H_
