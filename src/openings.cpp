a15e6e3862c127273ac5743a7f363373e2c1ec13
Mykolas Juraitis <mykolas@engineeredarts.co.uk>
2024-03-05 21:35:58 +0000

    chore: add files for openings DB

---
+++ src/openings.cpp
@@ -0,0 +1,5 @@
+#include "openings.hpp"
+
+Openings::Openings() {
+  // TODO: load
+}
---
+++ src/openings.hpp
@@ -0,0 +1,9 @@
+#ifndef OPENINGS_H_
+#define OPENINGS_H_
+
+class Openings {
+ public:
+  Openings();
+};
+
+#endif  // OPENINGS_H_
