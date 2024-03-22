73b5a78e9e2bf66f75468434a4c2d9fd68337d48
Mykolas Juraitis <mykolas@engineeredarts.co.uk>
2024-03-22 07:12:40 +0000

    test: add unit test for Universal Chess Interface

---
+++ test/uci_test.hpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "../src/uci.hpp"
+
+TEST(UCITest, InitialPosition) {
+  char const *uci_engine_argv[] = {
+    "/usr/games/stockfish", nullptr
+  };
+  UniversalChessInterface uci(uci_engine_argv, [&](std::string best_move) {
+  });
+
+  chess::Position position;
+
+  auto result = uci.evaluate_moves(position, position.generate_legal_moves());
+
+  EXPECT_EQ(result.size(), 20);
+}
--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -1,3 +1,4 @@
 #include "chess_engine_test.hpp"
 #include "openings_test.hpp"
 #include "video_capture_test.hpp"
+#include "uci_test.hpp"
